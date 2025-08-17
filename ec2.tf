64780d7f9b71dcc7001f69ba05242110488df9c9
unknown <jay.panicker91@gmail.com>
2025-08-17 14:08:10 +0100

    removed aws content

--- ec2.tf
+++
@@ -1,8 +0,0 @@
-# resource "aws_instance" "web" {
-#   ami                     = "ami-0ef9e689241f0bb6e"
-#   instance_type           = "t3.micro"
-
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
